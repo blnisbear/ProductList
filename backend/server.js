3923dc81ce36f27a5e7bb246ff60ab51fbbac2a2
liondev_ <bitelion9909@gmail.com>
2025-03-28 10:50:47 +0700

    edit fix server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
