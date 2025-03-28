f0162be1dc0a1ada7f001b46e180cce28bc3b574
liondev_ <bitelion9909@gmail.com>
2025-03-28 11:17:38 +0700

    Fix Server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
